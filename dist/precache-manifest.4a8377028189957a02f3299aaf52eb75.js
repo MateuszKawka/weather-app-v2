ecb5c8d2dd799ba027999fe7b45d71c8f8819041
Mateusz Kawka <kaawkamateusz@gmail.com>
2018-10-26 01:11:28 +0200

    Initial dist subtree commit

---
+++ dist/precache-manifest.4a8377028189957a02f3299aaf52eb75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "919c965ed60041c45cc3",
+    "url": "/js/chunk-vendors.20663088.js"
+  },
+  {
+    "revision": "76de282854d7c506783c",
+    "url": "/js/app.469a6856.js"
+  },
+  {
+    "revision": "a1cee3bbb7a1e5cb9b965e34a6e38b5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af8d4b4a149e6e6608761e075a09cd72",
+    "url": "/img/moon.af8d4b4a.svg"
+  },
+  {
+    "revision": "76de282854d7c506783c",
+    "url": "/css/app.26d63c80.css"
+  }
+];
