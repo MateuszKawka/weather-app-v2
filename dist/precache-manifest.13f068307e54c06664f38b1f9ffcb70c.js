69f3ecd6b69ec5a4c9586c90adaa7153bc527ed7
Mateusz Kawka <kaawkamateusz@gmail.com>
2018-10-26 01:16:03 +0200

    Initial dist subtree commit

---
+++ dist/precache-manifest.13f068307e54c06664f38b1f9ffcb70c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/weather-app-v2/robots.txt"
+  },
+  {
+    "revision": "919c965ed60041c45cc3",
+    "url": "/weather-app-v2/js/chunk-vendors.20663088.js"
+  },
+  {
+    "revision": "679fe6f4394a52e3b38e",
+    "url": "/weather-app-v2/js/app.75b659f8.js"
+  },
+  {
+    "revision": "9e41a05e8a6a7ab9840e914d23c10362",
+    "url": "/weather-app-v2/index.html"
+  },
+  {
+    "revision": "af8d4b4a149e6e6608761e075a09cd72",
+    "url": "/weather-app-v2/img/moon.af8d4b4a.svg"
+  },
+  {
+    "revision": "679fe6f4394a52e3b38e",
+    "url": "/weather-app-v2/css/app.b7e35c4e.css"
+  }
+];
