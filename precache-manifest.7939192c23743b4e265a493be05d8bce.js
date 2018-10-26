20d472f5eb85f95ec1fba483a47dc53074d4d1d2
Mateusz Kawka <kaawkamateusz@gmail.com>
2018-10-26 23:20:32 +0200

    Initial dist subtree commit

---
+++ precache-manifest.7939192c23743b4e265a493be05d8bce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/weather-app-v2/robots.txt"
+  },
+  {
+    "revision": "919c965ed60041c45cc3",
+    "url": "/weather-app-v2/js/chunk-vendors.20663088.js"
+  },
+  {
+    "revision": "57580723e6592f2b856d",
+    "url": "/weather-app-v2/js/app.827caa7b.js"
+  },
+  {
+    "revision": "e4bfd01605799384c458962bd633f0c2",
+    "url": "/weather-app-v2/index.html"
+  },
+  {
+    "revision": "af8d4b4a149e6e6608761e075a09cd72",
+    "url": "/weather-app-v2/img/moon.af8d4b4a.svg"
+  },
+  {
+    "revision": "57580723e6592f2b856d",
+    "url": "/weather-app-v2/css/app.cb5ee2f3.css"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/weather-app-v2/precache-manifest.bc783bde7a530d6765ab9bcf7af48ebf.js"
+  "/weather-app-v2/precache-manifest.7939192c23743b4e265a493be05d8bce.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "weather-app"});
